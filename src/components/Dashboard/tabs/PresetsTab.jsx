b9df6529910e34577ef96a882f3d268b41fc3454
O Seca Adegas <osecaadegas95@gmail.com>
2026-01-12 22:16:42 +0000

    Delete Dashboard folder (was overlay-only)

--- src/components/Dashboard/tabs/PresetsTab.jsx
+++
@@ -1,8 +0,0 @@
-export default function PresetsTab({ onLoadPreset }) {
-  return (
-    <div className="tab-content">
-      <h3>Overlay Presets</h3>
-      <p>Pre-made overlay configurations coming soon...</p>
-    </div>
-  );
-}
